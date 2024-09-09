88fcc43ac8694aae1d1dbcf26c44ce0d48610bb8
sowmyak <sowmya170204@gmail.com>
2024-09-09 07:42:52 +0530

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Upload from './Components/Upload';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Upload/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
